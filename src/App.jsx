a07280e2d3025f3b03c63b1a3f77dd319223fb86
No_otra_vez <saulbector@gmail.com>
2023-04-23 04:24:59 -0600

    New Branch to test materials

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,36 @@
 import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
-import { useMemo } from 'react'
-import { OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
-import { Perf } from 'r3f-perf'
+import { Stats, OrbitControls } from '@react-three/drei'
 
 export default function App() {
-  const polyhedron = useMemo(
-    () => [
-      new THREE.BoxGeometry(),
-      new THREE.SphereGeometry(0.785398),
-      new THREE.DodecahedronGeometry(0.785398)
-    ],
-    []
-  )
-
-  const options = useMemo(() => {
-    return {
-      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      visible: true,
-      wireframe: true,
-      color: { value: 'lime' }
-    }
-  }, [])
-
-  const pA = useControls('Polyhedron A', options)
-  const pB = useControls('Polyhedron B', options)
-
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
+    <Canvas camera={{ position: [-1, 4, 2.5] }}>
+      <directionalLight position={[1, 1, 1]} />
       <Polyhedron
-        position={[-1, 1, 0]}
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron}
-        wireframe={pA.wireframe}
+        name="meshBasicMaterial"
+        position={[-3, 1, 0]}
+        material={new THREE.MeshBasicMaterial()}
       />
       <Polyhedron
+        name="meshNormalMaterial"
+        position={[-1, 1, 0]}
+        material={new THREE.MeshNormalMaterial()}
+      />
+      <Polyhedron
+        name="meshPhongMaterial"
         position={[1, 1, 0]}
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron}
-        wireframe={pB.wireframe}
+        material={new THREE.MeshPhongMaterial()}
+      />
+      <Polyhedron
+        name="meshStandardMaterial"
+        position={[3, 1, 0]}
+        material={new THREE.MeshStandardMaterial()}
       />
       <OrbitControls target-y={1} />
       <axesHelper args={[5]} />
       <gridHelper />
-      <Perf position="bottom-left" minimal={true}/>
+      <Stats />
     </Canvas>
   )
 }
