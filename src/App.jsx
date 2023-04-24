4c6055fbcc02fe020b093368b559c1442ac0fce1
No_otra_vez <saulbector@gmail.com>
2023-04-23 22:32:55 -0600

    glTFLoader branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,29 @@
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
-import { useMemo } from 'react'
-import { OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
+import { Suspense } from 'react'
 import { Perf } from 'r3f-perf'
 
-export default function App() {
-  const polyhedron = useMemo(
-    () => [
-      new THREE.BoxGeometry(),
-      new THREE.SphereGeometry(0.785398),
-      new THREE.DodecahedronGeometry(0.785398)
-    ],
-    []
-  )
-
-  const options = useMemo(() => {
-    return {
-      x: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      y: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      z: { value: 0, min: 0, max: Math.PI * 2, step: 0.01 },
-      visible: true,
-      wireframe: true,
-      color: { value: 'lime' }
-    }
-  }, [])
-
-  const pA = useControls('Polyhedron A', options)
-  const pB = useControls('Polyhedron B', options)
-
+function Model() {
+  const url = require('./models/paricutin.glb')
+  const gltf = useGLTF(url)
+  console.log(gltf)
   return (
-    <Canvas camera={{ position: [1, 2, 3] }}>
-      <Polyhedron
-        position={[-1, 1, 0]}
-        rotation={[pA.x, pA.y, pA.z]}
-        visible={pA.visible}
-        color={pA.color}
-        polyhedron={polyhedron}
-        wireframe={pA.wireframe}
+    <Suspense>
+      <primitive
+        object={gltf.scene}
+        position={[0, 0, 0]}
+        scale={[10, 10, 10]}
       />
-      <Polyhedron
-        position={[1, 1, 0]}
-        rotation={[pB.x, pB.y, pB.z]}
-        visible={pB.visible}
-        color={pB.color}
-        polyhedron={polyhedron}
-        wireframe={pB.wireframe}
-      />
-      <OrbitControls target-y={1} />
-      <axesHelper args={[5]} />
-      <gridHelper />
-      <Perf position="bottom-left" minimal={true}/>
+    </Suspense>
+  )
+}
+export default function App() {
+  return (
+    <Canvas camera={{ position: [1, 0, 1] }} shadows>
+      <ambientLight intensity={40} />
+      <Model />
+      <OrbitControls target={[0, 0.3, 0]} minDistance={1.5} maxDistance={3}/>
+      <Perf position="top-left" />
     </Canvas>
   )
 }
