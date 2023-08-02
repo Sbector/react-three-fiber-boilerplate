5646ce2c053fdd36e86ad27b7bdb47723f6d4651
No_otra_vez <saulbector@gmail.com>
2023-08-02 02:10:08 -0600

    linearToneMapping

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import { Perf } from 'r3f-perf'
+import { ACESFilmicToneMapping, LinearToneMapping } from 'three';
 
 function Model() {
   const url = require('./models/paricutin.glb')
@@ -19,7 +20,7 @@ function Model() {
 }
 export default function App() {
   return (
-    <Canvas camera={{ position: [1, 0, 1] }} shadows>
+    <Canvas camera={{ position: [1, 0, 1] }} gl={{ toneMapping: LinearToneMapping, toneMappingExposure: 1.5 }}>
       <ambientLight intensity={40} />
       <Model />
       <OrbitControls target={[0, 0.3, 0]} minDistance={1} maxDistance={3}/>
